12e212cc3cafdfd6e7f8041fdb48c22d57dfc7a8
康正锋 <xzhm00@163.com>
2021-11-26 17:31:32 +0800

    创建一个Spring Boot Web应用

---
+++ boot-web-example/src/main/java/MyApplication.java
@@ -0,0 +1,25 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * MyApplication class
+ *
+ * @author 学渣很忙
+ * @date 2021/11/26
+ */
+@RestController
+@EnableAutoConfiguration
+public class MyApplication {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class, args);
+    }
+}
+
